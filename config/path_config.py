464e8ec45afe69d69baa4803ef98193d21be3347
George Victor <id0ubl3g@gmail.com>
2024-10-28 10:46:36 -0300

    feat: configure project root path, requirements, and server initialization

--- config/path_config.py
+++ config/path_config.py
@@ -3,11 +3,3 @@ import os
 
 def add_project_root_to_path():
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-
-
-add_project_root_to_path()
-
-from src.api.app import Server
-
-sv = Server()
-sv.run_development()
