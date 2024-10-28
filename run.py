5237c2c0821d5e0a0503343b9f2ec0634c08349f
George Victor <id0ubl3g@gmail.com>
2024-10-28 11:36:07 -0300

    feat: automate installation of LibreOffice

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
+from src.utils.installer import *
 from config.path_config import *
 from src.api.app import Server
 
 add_project_root_to_path()
 
 if __name__ == '__main__':
+    if not check_and_install_libreoffice():
+        exit(1)
+        
     server = Server()
     server.run_development()
